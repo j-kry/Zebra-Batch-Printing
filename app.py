3ed5f0899c6027a746ffb3a5bd753a9137843ae2
Justin <jkrysinski@icloud.com>
2024-08-02 09:07:54 -0500

    Cleaned up and saved label
    Cleaned up old code and comments. Readded the copied to clipboard label.
    Prepped adding sqlite db by adding log area and button

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-# tkinter enter lower bound and upper bound
-# loop to make each label templateH1 + assetNum + templateH2
-# concat each label to the main string
-
-### Update###
-# Change the second input box to a quantity
-# Enter starting tag and quantity to print
-
-# from tkinter.filedialog import asksaveasfilename
 import tkinter as tk
-import pyautogui as pag
-from subprocess import Popen
-import time
 import pyperclip as pc
 
-
-def DoTheThing(event):
+# Main Program Function
+def GenerateLabels(event):
+   #  Reset the Error Label
     errLbl.configure(text="")
+   #  Check that the entered starting number and quantity are both numbers greater than 0
     if (startEnt.get().isnumeric() and qEnt.get().isnumeric()):
         if (int(qEnt.get()) > 0):
 
@@ -31,61 +21,31 @@ def DoTheThing(event):
                 output += (templateH1 + str(prefixEnt.get()) +
                            incNum + templ2)
 
-            # FileSave(output)
-            zPrint(output)
+            # Copy the generated output to the clipboard
+            PrepareOutput(output)
         else:
             errLbl.configure(fg=("#AA0000"),
                              text="Quantity must be greater than 0 😡")
     else:
         errLbl.configure(fg=("#AA0000"),
                          text="Only enter whole numbers 😡")
+        
+def PrepareOutput(text):
+   pc.copy(text)
+   savedLbl.configure(fg=("#00AA00"), text="Copied to clipboard!")
+   # Wait 3 seconds and then hide the label again
+   window.after(3000, lambda:savedLbl.config(text=""))
+   
 
-
-# def SavePath():
-#     # Path = the one from the window
-#     filePath = asksaveasfilename(filetypes=[("Zebra Printer File", "*.prn")])
-
-#     # If filePath empty then return
-#     if not filePath:
-#         return
-
-#     return (filePath + ".prn")
-
-
-# def FileSave(text):
-#     try:
-#         path = SavePath()
-#         f = open(path, "x")
-#         f.write(text)
-#         f.close()
-#         savedLbl.configure(text_color="green",
-#                            text=f"Successfully saved to \"{path}\"")
-#     except:
-#         print("There is already a file with that name!!")
-
-# Added 02-27-2023
-# Need to run compiled program as admin
-
-
-def zPrint(text):
-    pc.copy(text)
-    print(pag.size())
-    Popen(['C:\Program Files (x86)\Zebra Technologies\Zebra Setup Utilities\App\PrnUtils'])
-    time.sleep(1)  # Wait a second
-    pag.moveTo(600, 315)  # Move to printer
-    pag.leftClick()
-    pag.moveTo(1100, 675)  # Move to open communication
-    pag.leftClick()
-    pag.hotkey('ctrl', 'v')
-
-
+# Template 1 is for the large printer and template 2 is for the smaller printer
 templateH1 = "^XA^FO45,30^A0,50,80^FDProperty of Thresholds^FS^FO45,90^BY4^BCN,80,Y,N,N,N^FD"
 templateH2 = "^FS^FO480,90^A0,35^FD(773) - 572 - 5399^FS^FO530,140^A0,40,60^FDOption 1^FS^XZ"
 templateH2SmallPrinter = "^FS^FO495,90^A0,35^FD(773) - 572 - 5399^FS^FO530,140^A0,40,60^FDOption 1^FS^XZ"
 
+# Create the window and add elements to the interface
 window = tk.Tk()
-window.geometry("500x400")
-window.title("Zebra .PRN File Generator")
+window.geometry("500x600")
+window.title("Zebra Batch Label Generator")
 
 errLbl = tk.Label(master=window, text="")
 errLbl.pack()
@@ -100,24 +60,34 @@ startLbl = tk.Label(master=window, text="Enter the starting label #")
 startLbl.pack()
 
 startEnt = tk.Entry(master=window, text="Start")
-startEnt.bind('<Return>', DoTheThing)
+startEnt.bind('<Return>', GenerateLabels)
 startEnt.pack()
 
 qLbl = tk.Label(master=window, text="How many labels to print?")
 qLbl.pack()
 
 qEnt = tk.Entry(master=window, text="Quantity")
-qEnt.bind('<Return>', DoTheThing)
+qEnt.bind('<Return>', GenerateLabels)
 qEnt.pack()
 
 smallPrintCheck = tk.Checkbutton(master=window, text='If using the smaller printer please check this', onvalue=1, offvalue=0)
 smallPrintCheck.pack()
 
-printBtn = tk.Button(master=window, text="Start Print Job", width=15,)
-printBtn.bind('<Button-1>', DoTheThing)
+printBtn = tk.Button(master=window, text="Generate Labels", width=15,)
+printBtn.bind('<Button-1>', GenerateLabels)
 printBtn.pack(pady=25)
 
-# savedLbl = tk.Label(master=window, text="", wraplength=250)
-# savedLbl.pack(pady=5)
+savedLbl = tk.Label(master=window, text="")
+savedLbl.pack(pady=1)
+
+labelCodeLbl = tk.Label(master=window, text="Last 10 Prints")
+labelCodeLbl.pack()
+
+labelCode = tk.Text(master=window, bd="3", height=10, width=20)
+labelCode.pack()
+
+viewLogBtn = tk.Button(master=window, text="View Full Log", width=15)
+viewLogBtn.pack()
+
 
 window.mainloop()
